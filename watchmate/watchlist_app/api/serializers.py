a663e659c3a413d45627d2cf11d4aa88ea85e9b9
Dinesh Anbazhagan <itzmidinesh@gmail.com>
2023-06-20 16:07:18 +0530

    validate field level in serializers

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -15,3 +15,9 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+    
+    def validate_name(self, value):
+        if len(value) <2:
+            raise serializers.ValidationError('Name must be at least 2 characters long')
+        else:
+            return value
