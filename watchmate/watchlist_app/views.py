fd259780dbb42bbc647659ba2c677d7d1f5fe8ff
Dinesh Anbazhagan <itzmidinesh@gmail.com>
2023-06-20 11:27:46 +0530

    create api and serialize

--- watchmate/watchlist_app/views.py
+++ watchmate/watchlist_app/views.py
@@ -1,19 +1,19 @@
-from django.shortcuts import render
-from .models import Movie
-from django.http import JsonResponse
-# Create your views here.
-def movie_list(request):
-    movies = Movie.objects.all()
-    context = {
-        'movies': list(movies.values())
-    }
-    return JsonResponse(context)
+# from django.shortcuts import render
+# from .models import Movie
+# from django.http import JsonResponse
+# # Create your views here.
+# def movie_list(request):
+#     movies = Movie.objects.all()
+#     context = {
+#         'movies': list(movies.values())
+#     }
+#     return JsonResponse(context)
 
-def movie_detail(request, pk):
-    movie =Movie.objects.get(pk=pk)
-    context = {
-        'name': movie.name,
-        'description': movie.description,
-        'active': movie.active
-    }
-    return JsonResponse(context)
+# def movie_detail(request, pk):
+#     movie =Movie.objects.get(pk=pk)
+#     context = {
+#         'name': movie.name,
+#         'description': movie.description,
+#         'active': movie.active
+#     }
+#     return JsonResponse(context)
