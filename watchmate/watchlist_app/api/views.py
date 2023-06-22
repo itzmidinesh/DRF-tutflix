e71773d7654a01af87ed90612e71b047c07c5d78
Dinesh Anbazhagan <itzmidinesh@gmail.com>
2023-06-22 18:34:16 +0530

    update view for streamplatform to include put and delete

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -25,6 +25,23 @@ class StreamDetailAV(APIView):
             return Response({'error': 'Streaming service not found'}, status=status.HTTP_404_NOT_FOUND)
         serializer = StreamPlatformSerializer(platform)
         return Response(serializer.data)
+    def put(self, request, pk):
+        try:
+            platform = StreamPlatform.objects.get(pk=pk)
+        except StreamPlatform.DoesNotExist:
+            return Response({'error': 'Streaming service not found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = StreamPlatformSerializer(platform, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk):
+        try:
+            platform = StreamPlatform.objects.get(pk=pk)
+        except StreamPlatform.DoesNotExist:
+            return Response({'error': 'Streaming service not found'}, status=status.HTTP_404_NOT_FOUND)
+        platform.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class WatchListAV(APIView):
     def get(self, request):
