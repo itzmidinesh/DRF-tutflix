5f59e203974c34e83da612065414a794fb32334d
Dinesh Anbazhagan <itzmidinesh@gmail.com>
2023-06-30 17:52:09 +0530

    update views with Mixins and GenericAPIView and urls.py

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 # from .views import movie_list, movie_detail
-from .views import WatchListAV, WatchDetailAV, StreamPlatformAV, StreamDetailAV
+from .views import WatchListAV, WatchDetailAV, StreamPlatformAV, StreamDetailAV, ReviewList, ReviewDetail
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name="movie-list"),
     path("<int:pk>", WatchDetailAV.as_view(), name="movie-detail"),
     path("stream/", StreamPlatformAV.as_view(), name="stream-list"),
-    path("stream/<int:pk>", StreamDetailAV.as_view(), name='streamplatform-detail'),
+    path("stream/<int:pk>/", StreamDetailAV.as_view(), name='streamplatform-detail'),
+    path('review/', ReviewList.as_view(), name="review-list"),
+    path('review/<int:pk>/', ReviewDetail.as_view(), name='review-detail')
 ]
