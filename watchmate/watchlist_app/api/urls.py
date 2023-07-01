8b7edada735803789170cbb332d0741536fb2fab
Dinesh Anbazhagan <itzmidinesh@gmail.com>
2023-07-01 23:28:48 +0530

    Router update in urls.py

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,14 +1,21 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 # from .views import movie_list, movie_detail
 from .views import (WatchListAV, WatchDetailAV,
-                    StreamPlatformAV, StreamPlatformDetailAV,
+                    StreamPlatformVS,
+                    StreamPlatformAV,
+                    StreamPlatformDetailAV,
                     ReviewList, ReviewDetail, ReviewCreate)
+
+router = DefaultRouter()
+router.register('stream', StreamPlatformVS, basename='streamplatform')
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name="movie-list"),
     path("<int:pk>/", WatchDetailAV.as_view(), name="movie-detail"),
-    path("stream/", StreamPlatformAV.as_view(), name="stream-list"),
-    path("stream/<int:pk>/", StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),    
+    path('', include(router.urls)),
+    # path("stream/", StreamPlatformAV.as_view(), name="stream-list"),
+    # path("stream/<int:pk>/", StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),    
     # path("review/", ReviewList.as_view(), name="review-list"),
     # path("review/<int:pk>/", ReviewDetail.as_view(), name='review-detail'),
     
