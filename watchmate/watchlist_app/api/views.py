8a88b46d0cffae595c4f63a1dc7c62673ffb0f7b
Dinesh Anbazhagan <itzmidinesh@gmail.com>
2023-07-03 21:57:07 +0530

    change 1 review per user per movie

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 # from rest_framework.decorators import api_view
+from rest_framework.exceptions import ValidationError
 from rest_framework.views import APIView
 from rest_framework import status, generics, mixins, viewsets
 from watchlist_app.models import WatchList, StreamPlatform, Review
@@ -11,10 +12,16 @@ from watchlist_app.api.serializers import (WatchListSerializer,
 class ReviewCreate(generics.CreateAPIView):
     serializer_class = ReviewSerializer
     
+    def get_queryset(self):
+        return Review.objects.all()
     def perform_create(self, serializer):
         pk = self.kwargs.get('pk')
         movie = WatchList.objects.get(pk=pk)
-        serializer.save(watchlist=movie)
+        review_user = self.request.user
+        review_queryset = Review.objects.filter(watchlist=movie, review_user=review_user)
+        if review_queryset.exists():
+            raise ValidationError("You have already reviewed this movie!")
+        serializer.save(watchlist=movie, review_user=review_user)
     
 class ReviewList(generics.ListAPIView):
     # queryset = Review.objects.all()
