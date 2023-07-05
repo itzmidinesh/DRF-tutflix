77d38a730a40253b3c3f917e4d52b2c8c6ccd120
Dinesh Anbazhagan <itzmidinesh@gmail.com>
2023-07-05 23:24:41 +0530

    add rating in models, remove stream in api.urls, update view to include rating

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -18,12 +18,18 @@ class ReviewCreate(generics.CreateAPIView):
         return Review.objects.all()
     def perform_create(self, serializer):
         pk = self.kwargs.get('pk')
-        movie = WatchList.objects.get(pk=pk)
+        watchlist = WatchList.objects.get(pk=pk)
         review_user = self.request.user
-        review_queryset = Review.objects.filter(watchlist=movie, review_user=review_user)
+        review_queryset = Review.objects.filter(watchlist=watchlist, review_user=review_user)
         if review_queryset.exists():
             raise ValidationError("You have already reviewed this movie!")
-        serializer.save(watchlist=movie, review_user=review_user)
+        if watchlist.number_rating == 0:
+            watchlist.avg_rating = serializer.validated_data["rating"]
+        else:
+            watchlist.avg_rating = (watchlist.avg_rating + serializer.validated_data["rating"])/2
+        watchlist.number_rating += 1
+        watchlist.save()
+        serializer.save(watchlist=watchlist, review_user=review_user)
     
 class ReviewList(generics.ListAPIView):
     # queryset = Review.objects.all()
